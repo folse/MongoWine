29b9b4e99ebeb26b3571f3323c71cd8dd6c48584
folse <folsee@gmail.com>
2016-09-16 17:32:54 +0800

    merge update_wine to get_wine

--- get_wine.py
+++ get_wine.py
@@ -65,6 +65,124 @@ def get_update_time_period():
 
 	return current_day_string + " " +  current_hour_string + ':00'
 
+def get_wine_info(wine_number, category):
+	url = 'https://api.systembolaget.se/V4/artikel/' + wine_number
+	username = 'DMZ1\SybApi'
+	password = 'zc3R21Q8nJ4y8Pj1A6uB'
+	send_headers = {
+	 'Host':'api.systembolaget.se',
+	 'User-Agent':'Systembolaget/2.1 (iPhone; iOS 10.0; Scale/2.00)',
+	 'Authorization':'Basic RE1aMVxTeWJBcGk6emMzUjIxUThuSjR5OFBqMUE2dUI=',
+	 'Connection':'keep-alive'
+	}
+	  
+	passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	passman.add_password(None, url, username, password)
+	urllib2.install_opener(urllib2.build_opener(urllib2.HTTPBasicAuthHandler(passman)))
+
+	req = urllib2.Request(url,headers=send_headers)
+
+	try:
+		resp = urllib2.urlopen(req).read()
+		parse_wine_info(resp, wine_number, category)
+	except urllib2.HTTPError, e:
+		print e
+		pass
+	except urllib2.socket.error, e:
+		print e
+		time.sleep(2)
+		resp = urllib2.urlopen(req).read()
+		parse_wine_info(resp, wine_number, category)
+
+def parse_wine_info(resp, wine_number, category):
+
+	data = json.loads(resp)
+
+	if data.has_key('Artikeldetaljer'):
+
+		wine_detail = data['Artikeldetaljer']
+
+		wine_info = ''
+    	sales_start = ''
+    	alcohol = ''
+    	color = ''
+    	fragrance = ''
+    	ingredient = ''
+    	sugar = ''
+    	producer = ''
+    	supplier = ''
+
+    	if data.has_key('Artiklar'):
+			for i in range(len(data['Artiklar'])):
+				wine_detail_number = data['Artiklar'][i]['ArtikelNr']
+				if wine_detail_number == wine_number:
+					wine_info = data['Artiklar'][i]
+
+			if wine_info.has_key('Saljstartsdatum'):
+				sales_start = get_date_from_timestamp(wine_info['Saljstartsdatum'])
+
+			if wine_info.has_key('Alkoholhalt'):
+				alcohol = str(wine_info['Alkoholhalt']) + ' %'
+
+			if wine_detail.has_key('Farg'):
+				color = wine_detail['Farg']
+
+			if wine_detail.has_key('Doft'):
+				fragrance = wine_detail['Doft']
+
+			if wine_detail.has_key('Ravara'):
+				ingredient = wine_detail['Ravara']
+
+			if wine_detail.has_key('Sockerhalt'):
+				sugar = wine_detail['Sockerhalt'] + ' g/l'
+
+			if wine_info.has_key('Producent'):
+				producer = wine_info['Producent']
+
+			if wine_info.has_key('Leverantor'):
+				supplier = wine_info['Leverantor']
+
+			inventory_collection = "inventory_" + wine_category
+
+			db[inventory_collection].update({ "wine_number": wine_number }, \
+											{ "$set": { "sales_start": sales_start, \
+														"alcohol": alcohol, \
+														"color": color, \
+														"fragrance": fragrance, \
+														"ingredient": ingredient, \
+														"sugar": sugar, \
+														"producer": producer, \
+														"supplier": supplier, \
+														}}, False, False)
+
+def get_date_from_timestamp(time_stamp_info):
+	
+	time_stamp = 0
+
+	if time_stamp_info[6] == '1':
+		time_stamp = int(time_stamp_info[6:16])
+	else:
+		time_stamp = int(time_stamp_info[6:15])
+
+	return time.strftime("%Y-%m-%d", time.gmtime(time_stamp + 7200))
+
+def update_wine():
+	wine_category_dict = { "red_wine": u'Rött vin', "white_wine": u'Vitt vin' }
+
+	for wine_category in wine_category_dict.keys():
+
+		inventory_collection = "inventory_" + wine_category
+
+		wine_numbers = []
+		for wine_number in db[inventory_collection].distinct('wine_number'):
+			wine_numbers.append(wine_number)
+
+		for wine_number in wine_numbers:
+			inventory = db[inventory_collection].find_one({ "wine_number": wine_number, "alcohol": {"$exists": False} })
+			if inventory != None:
+				print inventory['wine_number']
+				get_wine_info(inventory['wine_number'], wine_category)
+
 if __name__ == '__main__':
 
 	global db
@@ -85,4 +203,4 @@ if __name__ == '__main__':
 		for wine_category in wine_category_dict.keys():
 			get_store_wine(wine_category, sys_store_id, 0)
 
-	print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	update_wine()
